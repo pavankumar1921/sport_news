a2e2f4f250eab43262392b177567ce2d4427ef19
Pavan Kumar <pavankittu19@gmail.com>
2023-10-30 13:08:47 +0530

    added favourites for teams

--- src/pages/favourites/Favourites.tsx
+++ src/pages/favourites/Favourites.tsx
@@ -4,6 +4,8 @@ import { useArticlesState } from "../../context/articles/context";
 import ArticleDetails from "../articles/ArticleDetails";
 import { API_ENDPOINT } from "../../config/constants";
 import { useArticlesDispatch } from "../../context/articles/context";
+import { fetchTeams } from "../../context/teams/action";
+import { useTeamsDispatch } from "../../context/teams/context";
 
 export default function Favourites(){
     const state = useArticlesState()
@@ -25,8 +27,17 @@ export default function Favourites(){
         name:string
     }
 
-    const[sportsData,setSportsData] = useState<Sports[]>([])
-    const [selectedSport,setSelectedSport] = useState<number |null>(null)
+    type Teams = {
+        id:number
+        name: string
+        plays:string
+    }
+
+    const [sportsData,setSportsData] = useState<Sports[]>([])
+    const [selectedSport,setSelectedSport] = useState<number | null>(null)
+
+    const [teamsData,setTeamsData] = useState<Teams[]>([])
+    const [selectedTeam,setSelectedTeam] = useState<number | null>(null)
 
     useEffect(()=>{
         const fetchSportsData = async() =>{
@@ -36,7 +47,15 @@ export default function Favourites(){
             const data = await response.json()
             setSportsData(data.sports)
         }
+        const fetchTeamsData = async() =>{
+            const response = await fetch(`${API_ENDPOINT}/teams`,{
+                method: "GET"
+            })
+            const data = await response.json()
+            setTeamsData(data)
+        }
         fetchSportsData()
+        fetchTeamsData()
     },[])
 
     useEffect(()=>{
@@ -45,14 +64,27 @@ export default function Favourites(){
         }
     },[])
 
+    const dispatchTeams = useTeamsDispatch()
+    
+    useEffect(()=>{
+        if(selectedTeam != null) {
+            fetchTeams(dispatchTeams)
+        }
+    })
+
     const selectSport = (id:number |null) =>{
         setSelectedSport(id)
+        setSelectedTeam(null)
+    }
+    const selectTeam = (id:number | null )=>{
+        setSelectedTeam(id)
     }
 
     const filteredArticles= articles.filter((article:any)=>{
         if(selectedSport !== null) {
             const sportArticles = article.sport.id === selectedSport
-            return sportArticles
+            const teamArticles = selectedTeam === null || Object.values(article.teams).some((team:any) => team.id === selectedTeam)
+            return sportArticles && teamArticles
         }else{
             return true
         }
@@ -70,15 +102,32 @@ export default function Favourites(){
                     )
                 )}
                 </select>
-                <div className="flex flex-col gap-3">
-                {filteredArticles.map((article:any)=>(
-                    <div key={article.id} className="border rounded-lg shadow-lg p-4">
-                        <h2>{article.sport.name}</h2>
-                        <h2>{article.title}</h2>
-                        <p>{article.summary}</p>
-                        <div>{fullArticleDetails(article.id)}</div>
-                        </div>
+            {teamsData && (
+            <select name="teams" id="teams" onChange={(e) => selectTeam(parseInt(e.target.value, 10) || null)} className="p-2 border rounded-lg">
+                <option value="">Select team</option>
+                {teamsData
+                .filter(
+                (team) =>
+                selectedSport === null ||
+                sportsData.find((sport) => sport.id === selectedSport)?.name === team.plays
+                )
+                .map((team: any) => (
+                <option key={team.id} value={team.id}>
+                    {team.name}
+                </option>
                 ))}
+            </select>
+            )}
+
+                <div className="flex flex-col gap-3">
+                    {filteredArticles.map((article:any)=>(
+                        <div key={article.id} className="border rounded-lg shadow-lg p-4">
+                            <h2>{article.sport.name}</h2>
+                            <h2>{article.title}</h2>
+                            <p>{article.summary}</p>
+                            <div>{fullArticleDetails(article.id)}</div>
+                        </div>
+                    ))}
                 </div>
                 
                 </div>
