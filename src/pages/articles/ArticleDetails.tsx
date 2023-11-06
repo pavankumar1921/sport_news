95c499227003963c34e9ba4aac8594ae4a206084
Pavan Kumar <pavankittu19@gmail.com>
2023-11-06 19:51:03 +0530

    corrected ArticleListItems to fetch thumbnail in the article

--- src/pages/articles/ArticleDetails.tsx
+++ src/pages/articles/ArticleDetails.tsx
@@ -50,52 +50,82 @@ const ArticleDetails: React.FC<{id:number}>=({id}) =>{
             <button type="button" onClick={openModal}>More Details</button>
         </div>
         <div>
-            <Transition appear show={isOpen} as={Fragment}>
-                <Dialog as="div" className="relative z-10 items-center justify-center" onClose={closeModal}>
-                    <Transition.Child 
-                        as="div"
-                        className="fixed inset-0"
-                        enter="ease-out duration-300"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="ease-in duration-200"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0">
-                            <div className="inset-0 flex " />
-                            <div className="absolute inset-0 bg-black bg-opacity-70">
-                            <Dialog.Panel className="w-[80vw] h-[60vh] mx-auto rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all  overflow-hidden">
-                                {ArticleData && (
-                                    <>
-                                        <div>
-                                        <button onClick={closeModal} className="text-gray-500 hover:text-red-700">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                            </svg>
-                                        </button>
-                                        </div>
-                                        <div className="">
-                                            <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                                                {ArticleData.title}
-                                            </Dialog.Title>
-                                            <h2 >{ArticleData.sport.name}</h2>
-                                            <h2>{new Date(ArticleData.date).toLocaleDateString()}</h2>
-                                            <div className="w-1/2">
-                                            <img src={ArticleData.thumbnail} className="w-40 h-40 mx-auto rounded"/>
-                                            </div>
-                                            
-                                            
-                                            <p>{ArticleData.content}</p>
-                                        </div>
-
-                                    </>
-                                )}
-                            </Dialog.Panel>
-                            </div>
-                            
-                    </Transition.Child>
-                </Dialog>
-            </Transition>
-        </div>
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="fixed inset-0 flex items-center justify-center z-50"
+            onClose={closeModal}
+          >
+            <Transition.Child
+              as="div"
+              className="fixed inset-0"
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+            </Transition.Child>
+            <Transition.Child
+              as="div"
+              className="relative z-10"
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-60vw h-60vh mx-auto rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all overflow-hidden">
+                {ArticleData && (
+                  <>
+                    <div>
+                      <button
+                        onClick={closeModal}
+                        className="text-gray-500 hover:text-red-700"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke-width="1.5"
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                            d="M6 18L18 6M6 6l12 12"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                    <div className="">
+                      <Dialog.Title
+                        as="h3"
+                        className="text-lg font-medium leading-6 text-gray-900"
+                      >
+                        {ArticleData.title}
+                      </Dialog.Title>
+                      <h2>{ArticleData.sport.name}</h2>
+                      <h2>{new Date(ArticleData.date).toLocaleDateString()}</h2>
+                      <div className="w-1/2">
+                        <img
+                          src={ArticleData.thumbnail}
+                          className="w-40 h-40 mx-auto rounded"
+                        />
+                      </div>
+                      <p>{ArticleData.content}</p>
+                    </div>
+                  </>
+                )}
+              </Dialog.Panel>
+            </Transition.Child>
+          </Dialog>
+        </Transition>
+      </div>
         </>
         
         
--- src/pages/articles/ArticleListItems.tsx
+++ src/pages/articles/ArticleListItems.tsx
@@ -7,20 +7,22 @@ export default function ArticleListItems() {
   const { articles, isLoading, isError, errorMessage } = state;
   const [selectedSport, setSelectedSport] = useState("All");
   let [sports, setSports] = useState<string[]>([]);
-const [sortOrder,setSortOrder] = useState("date")
+  const [sortOrder, setSortOrder] = useState("date");
 
   useEffect(() => {
     if (articles) {
-      const uniqueSports = Array.from(new Set(articles.map((article: any) => article.sport.name)));
+      const uniqueSports = Array.from(
+        new Set(articles.map((article: any) => article.sport.name))
+      );
 
       setSports(uniqueSports);
     }
   }, [articles]);
 
-  const fullArticleDetails = (id:number) =>{
-    return <ArticleDetails id={id} />
-  }
-   const handleRugbyButtonClick = () => {
+  const fullArticleDetails = (id: number) => {
+    return <ArticleDetails id={id} />;
+  };
+  const handleRugbyButtonClick = () => {
     setSelectedSport("Rugby");
   };
   if (isLoading && articles.length === 0) {
@@ -32,27 +34,29 @@ const [sortOrder,setSortOrder] = useState("date")
   }
 
   const filteredArticles = articles
-  .filter((article) => selectedSport === "All" || article.sport.name === selectedSport
-  )
-  .sort((a,b)=>{
-    if(sortOrder === 'date'){
-      const dateA = new Date(a.date)
-      const dateB = new Date(b.date)
-      return dateB.getTime() - dateA.getTime()
-    }else if(sortOrder === 'title'){
-      return a.title.localeCompare(b.title)
-    }
-  })
+    .filter(
+      (article) =>
+        selectedSport === "All" || article.sport.name === selectedSport
+    )
+    .sort((a, b) => {
+      if (sortOrder === "date") {
+        const dateA = new Date(a.date);
+        const dateB = new Date(b.date);
+        return dateB.getTime() - dateA.getTime();
+      } else if (sortOrder === "title") {
+        return a.title.localeCompare(b.title);
+      }
+    });
   return (
     <div className="w-full p-4">
       <div className="mb-4">
         <label>Filter by Sport: </label>
         <div className="space-x-4">
-        <button
+          <button
             onClick={() => setSelectedSport("All")}
-            className={`px-4 py-2 rounded border ${selectedSport === "All" ? "border-green-500" : "border-gray-300"} ${
-              selectedSport === "All" ? "bg-green-100" : "bg-gray-100"
-            }`}
+            className={`px-4 py-2 rounded border ${
+              selectedSport === "All" ? "border-green-500" : "border-gray-300"
+            } ${selectedSport === "All" ? "bg-green-100" : "bg-gray-100"}`}
           >
             All
           </button>
@@ -60,45 +64,62 @@ const [sortOrder,setSortOrder] = useState("date")
             <button
               key={index}
               onClick={() => setSelectedSport(sport)}
-              className={`px-4 py-2 rounded border ${selectedSport === sport ? "border-green-500" : "border-gray-300"} ${
-                selectedSport === sport ? "bg-green-100" : "bg-gray-100"
-              }`}
+              className={`px-4 py-2 rounded border ${
+                selectedSport === sport ? "border-green-500" : "border-gray-300"
+              } ${selectedSport === sport ? "bg-green-100" : "bg-gray-100"}`}
             >
               {sport}
             </button>
-            
           ))}
           <button
             onClick={handleRugbyButtonClick}
-            className={`px-4 py-2 rounded border ${selectedSport === "Rugby" ? "border-green-500" : "border-gray-300"} ${
-              selectedSport === "Rugby" ? "bg-green-100" : "bg-gray-100"
-            }`}
+            className={`px-4 py-2 rounded border ${
+              selectedSport === "Rugby" ? "border-green-500" : "border-gray-300"
+            } ${selectedSport === "Rugby" ? "bg-green-100" : "bg-gray-100"}`}
           >
             Rugby
           </button>
           <label>Sort by:</label>
-        <select value={sortOrder} onChange={(e) => setSortOrder(e.target.value)}  className="px-4 py-2 rounded border border-gray-300">
-          <option value="date">Date</option>
-          <option value="title">Title</option>
-        </select>
+          <select
+            value={sortOrder}
+            onChange={(e) => setSortOrder(e.target.value)}
+            className="px-4 py-2 rounded border border-gray-300"
+          >
+            <option value="date">Date</option>
+            <option value="title">Title</option>
+          </select>
         </div>
       </div>
       <div className="flex flex-col gap-3">
         {filteredArticles && filteredArticles.length > 0 ? (
           filteredArticles
-            .filter((article: any) => selectedSport === "All" || article.sport.name === selectedSport)
+            .filter(
+              (article: any) =>
+                selectedSport === "All" || article.sport.name === selectedSport
+            )
             .map((article: any) => (
-              
               <div key={article.id} className="border rounded-lg shadow-lg p-4">
-                <h5 className="font-semibold">{article.sport.name}</h5>
-                <h4 className="text-gray-500">{article.title}</h4>
-                <h3>{new Date(article.date).toLocaleDateString("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                })}</h3>
-                <p>{article.summary}</p>
-                <div>{fullArticleDetails(article.id)}</div>
+                <div className="flex">
+                  <div className="w-2/3 pr-4">
+                    <h5 className="font-semibold">{article.sport.name}</h5>
+                    <h4 className="text-gray-500">{article.title}</h4>
+                    <h3>
+                      {new Date(article.date).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </h3>
+                    <p>{article.summary}</p>
+                    <div>{fullArticleDetails(article.id)}</div>
+                  </div>
+                  <div className="w-1/3">
+                    <img
+                      src={article.thumbnail}
+                      className="w-40 h-40 mx-auto rounded"
+                    />
+                  </div>
+                </div>
               </div>
             ))
         ) : (
