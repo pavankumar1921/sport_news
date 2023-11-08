cb0ae45a654ae43b595f58f46dde64f2712ea902
Pavan Kumar <pavankittu19@gmail.com>
2023-11-08 15:30:27 +0530

    added Link to home in Signin form

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import SigninForm from "./SigninForm";
+import { Link } from "react-router-dom";
 
 const Signin: React.FC = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <Link
+        to="/"
+        className="absolute top-4 left-4 bg-gray-500 hover:bg-gray-900 text-white px-4 py-2 rounded"
+      >
+        Home
+      </Link>
       <div className="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
           Sign in
--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -9,7 +9,7 @@ const Signup: React.FC = () => {
         to="/"
         className="absolute top-4 left-4 bg-gray-500 hover:bg-gray-900 text-white px-4 py-2 rounded"
       >
-        Go Back
+        Home
       </Link>
       <div className="max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
         <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">
